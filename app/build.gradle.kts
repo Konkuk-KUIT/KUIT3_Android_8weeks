f6174fa60c3f6741ecb896c04b9cba0871c869ce
Kwon <seaweed8759@naver.com>
2024-05-25 02:00:41 +0900

    8주차 미션 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,12 @@ dependencies {
      *
      *
      * **/
-
+    implementation("com.squareup.retrofit2:retrofit:2.6.4")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.4")
+    implementation("com.squareup.retrofit2:converter-scalars:2.6.4")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
+    implementation("com.github.bumptech.glide:glide:4.13.2")
 
 
 }
